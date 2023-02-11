d39d42d40fbfd7752ee49660b7b5b844a342e6d4
je-delray <hovax74935@ekcsoft.com>
2023-02-11 10:53:34 +0100

    third

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-import { menuArray } from ".data.js";
+import menuArray from "./data.js";
+console.log(menuArray);
+
+const BtnPizza = document.getElementById("addPizza");
+const BtnHamburger = document.getElementById("addHamburger");
+const BtnBeer = document.getElementById("addBeer");
+
+const orderList = document.getElementsByClassName("order-list");
+
+let orderData = "";
+
+addPizzaBtn.addEventListener("click", function () {
+  const plusPizza = (orderList.innerHTML += orderData += plusPizza);
+});
+
+addHamburgerBtn.addEventListener("click", function () {});
+
+addBeerBtn.addEventListener("click", function () {});
