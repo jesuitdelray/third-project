93b6da5df34b382ab09dfeb16dab0bfb2bb83558
je-delray <hovax74935@ekcsoft.com>
2023-02-11 13:16:54 +0100

    changes

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-import menuArray from "./data.js";
-console.log(menuArray);
+const menuArray = [
+  {
+    name: "Pizza",
+    ingredients: ["pepperoni", "mushrom", "mozarella"],
+    price: 14,
+    emoji: "🍕",
+    id: 0,
+  },
+  {
+    name: "Hamburger",
+    ingredients: ["beef", "cheese", "lettuce"],
+    price: 12,
+    emoji: "🍔",
+    id: 1,
+  },
+  {
+    name: "Beer",
+    ingredients: ["grain, hops, yeast, water"],
+    price: 12,
+    emoji: "🍺",
+    id: 2,
+  },
+];
 
 const BtnPizza = document.getElementById("addPizza");
 const BtnHamburger = document.getElementById("addHamburger");
@@ -7,12 +28,38 @@ const BtnBeer = document.getElementById("addBeer");
 
 const orderList = document.getElementsByClassName("order-list");
 
-let orderData = "";
+const plusPizza = `<div class="order-name">
+<h4>${menuArray[0].name}</h4>
+<h4>${menuArray[0].price}</h4>
+</div>`;
+
+const plusHamburger = `<div class="order-name">
+<h4>${menuArray[1].name}</h4>
+<h4>${menuArray[1].price}</h4>
+</div>`;
+
+const plusBeer = `<div class="order-name">
+<h4>${menuArray[2].name}</h4>
+<h4>${menuArray[2].price}</h4>
+</div>`;
+
+let orderData = ``;
 
 addPizzaBtn.addEventListener("click", function () {
-  const plusPizza = (orderList.innerHTML += orderData += plusPizza);
+  orderData += plusPizza;
+  render();
 });
 
-addHamburgerBtn.addEventListener("click", function () {});
+addHamburgerBtn.addEventListener("click", function () {
+  orderData += plusHamburger;
+  render();
+});
 
-addBeerBtn.addEventListener("click", function () {});
+addBeerBtn.addEventListener("click", function () {
+  orderData += plusBeer;
+  render();
+});
+
+function render() {
+  orderList.innerHTML += orderData;
+}
