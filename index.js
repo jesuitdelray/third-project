eba9f83a9c5c1891df85736c07d2eda2d3477f05
je-delray <hovax74935@ekcsoft.com>
2023-02-12 15:20:17 +0100

    changed

--- index.js
+++ index.js
@@ -22,44 +22,137 @@ const menuArray = [
   },
 ];
 
-const BtnPizza = document.getElementById("addPizza");
-const BtnHamburger = document.getElementById("addHamburger");
-const BtnBeer = document.getElementById("addBeer");
+const addPizzaBtn = document.getElementById("addPizza");
+const addHamburgerBtn = document.getElementById("addHamburger");
+const addBeerBtn = document.getElementById("addBeer");
 
-const orderList = document.getElementsByClassName("order-list");
+const orderListEL = document.getElementById("orderList");
+const orderEl = document.getElementById("order");
+const totalEl = document.getElementById("totalPrice");
+const orderBtnEl = document.getElementById("orderButton");
+let orderBtnExist = false;
 
-const plusPizza = `<div class="order-name">
-<h4>${menuArray[0].name}</h4>
-<h4>${menuArray[0].price}</h4>
-</div>`;
+let count = {
+  pizza: 0,
+  hamburger: 0,
+  beer: 0,
+};
 
-const plusHamburger = `<div class="order-name">
-<h4>${menuArray[1].name}</h4>
-<h4>${menuArray[1].price}</h4>
-</div>`;
+let price = {
+  pizza: menuArray[0].price,
+  hamburger: menuArray[1].price,
+  beer: menuArray[2].price,
+};
 
-const plusBeer = `<div class="order-name">
-<h4>${menuArray[2].name}</h4>
-<h4>${menuArray[2].price}</h4>
-</div>`;
+let totalPizza = 0;
+let totalHamburger = 0;
+let totalBeer = 0;
 
-let orderData = ``;
+let totalPrice = 0;
 
 addPizzaBtn.addEventListener("click", function () {
-  orderData += plusPizza;
-  render();
+  count.pizza++;
+  totalPizza = count.pizza * price.pizza;
+  updateTotal();
+  renderOrder();
 });
 
 addHamburgerBtn.addEventListener("click", function () {
-  orderData += plusHamburger;
-  render();
+  count.hamburger++;
+  totalHamburger = count.hamburger * price.hamburger;
+  updateTotal();
+  renderOrder();
 });
 
 addBeerBtn.addEventListener("click", function () {
-  orderData += plusBeer;
-  render();
+  count.beer++;
+  totalBeer = count.beer * price.beer;
+  updateTotal();
+  renderOrder();
 });
 
-function render() {
-  orderList.innerHTML += orderData;
+if (orderBtnExist === true) {
+  orderBtnEl.addEventListener("click", function () {
+    console.log("working");
+  });
 }
+
+function renderOrder() {
+  const pizzaHtml = `<div class="order-name">
+  <p>${menuArray[0].name}</p>
+  <p>${totalPizza}</p>
+  </div>`;
+
+  const hamburgerHtml = `<div class="order-name">
+  <p>${menuArray[1].name}</p>
+  <p>${totalHamburger}</p>
+  </div>`;
+
+  const beerHtml = `<div class="order-name">
+  <p>${menuArray[2].name}</p>
+  <p>${totalBeer}</p>
+  </div>`;
+
+  const totalHtml = `<div class="total-price">
+  <p>Total price:</p>
+  <p>${totalPrice}</p>
+  </div>`;
+
+  const completeBtnHtml = `</div>
+  <button id="orderButton">Complete order</button></div>`;
+
+  if (totalPrice) {
+    orderEl.innerHTML =
+      pizzaHtml + hamburgerHtml + beerHtml + totalHtml + completeBtnHtml;
+    orderBtnExist = true;
+  }
+}
+
+function updateTotal() {
+  totalPrice = totalPizza + totalHamburger + totalBeer;
+}
+// let pizzaPrice = menuArray[0].price
+// let hamburgerPrice = menuArray[1].price
+// let beerPrice = menuArray[2].price
+
+// let plusPizza = `<div class="order-name">
+// <p>${menuArray[0].name}</p>
+// <p>${pizzaPrice}</p>
+// </div>`;
+
+// let plusHamburger = `<div class="order-name">
+// <p>${menuArray[1].name}</p>
+// <p>${hamburgerPrice}</p>
+// </div>`;
+
+// let plusBeer = `<div class="order-name">
+// <p>${menuArray[2].name}</p>
+// <p>${beerPrice}</p>
+// </div>`;
+
+// let orderData = ``;
+
+// addPizzaBtn.addEventListener("click", function () {
+//   if(!orderData) {
+//   orderData += plusPizza;
+//   render()
+// } else {
+//   plusPizza.filter(function(){
+//     'pizza'
+//   })
+//   render();
+// }});
+
+// addHamburgerBtn.addEventListener("click", function () {
+//   orderData += plusHamburger;
+//   render();
+// });
+
+// addBeerBtn.addEventListener("click", function () {
+//   orderData += plusBeer;
+//   render();
+// });
+
+// function render() {
+//   orderList.innerHTML += orderData;
+// }
